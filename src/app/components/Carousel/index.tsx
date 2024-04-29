acd29da9a6c46840c6e5103a1be30dde178fc7f4
Natálie Sedláková <natalie.sedlakova@betrian.cz>
2024-04-29 12:43:51 +0200

    feature: pause carousel

--- src/app/components/Carousel/index.tsx
+++ src/app/components/Carousel/index.tsx
@@ -1,48 +1,56 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { Button, ButtonsWrapper, CarouselWrapper, Slider, Wrapper } from './styledComponents';
 import { authors } from 'app/content/authors';
 import { Card } from './Card';
 
 export const Carousel = () => {
   const [index, setIndex] = useState(0);
+  const [play, setPlay] = useState(true);
 
   const handlePrevious = () => {
     const newIndex = index - 1;
     setIndex(newIndex < 0 ? authors.length - 3 : newIndex);
   };
 
-  const handleNext = () => {
-    const newIndex = index + 1;
-    setIndex(newIndex > authors.length - 3 ? 0 : newIndex);
-  };
+  const handleNext = useCallback(() => {
+    if (play) {
+      const newIndex = index + 1;
+      setIndex(newIndex > authors.length - 3 ? 0 : newIndex);
+    }
+  }, [index, play]);
 
   useEffect(() => {
-    const timeoutFunction = setTimeout(() => handleNext(), 5000);
+    const timeoutFunction = setTimeout(handleNext, 1000);
     return () => clearTimeout(timeoutFunction);
-  }, [index]);
+  }, [index, play]);
 
-  console.log(index);
+  console.log(play);
 
   return (
-    <Wrapper id="authors_carousel">
-      <CarouselWrapper>
-        <Slider
-          style={{
-            gridTemplateColumns: `repeat(${authors.length}, 1fr)`,
-            width: `${authors.length * 33.33}%`,
-            translate: `${-(index * (100 / authors.length))}%`,
-          }}
-        >
-          {authors.map((author) => (
-            <Card key={`${author.name}`} author={author} />
-          ))}
-        </Slider>
-      </CarouselWrapper>
+    <>
+      <div color="black">{play}</div>
+      <Wrapper id="authors_carousel">
+        <CarouselWrapper>
+          <Slider
+            style={{
+              gridTemplateColumns: `repeat(${authors.length}, 1fr)`,
+              width: `${authors.length * 33.33}%`,
+              translate: `${-(index * (100 / authors.length))}%`,
+            }}
+            onMouseEnter={() => setPlay(false)}
+            onMouseLeave={() => setPlay(true)}
+          >
+            {authors.map((author) => (
+              <Card key={`${author.name}`} author={author} />
+            ))}
+          </Slider>
+        </CarouselWrapper>
 
-      <ButtonsWrapper>
-        <Button onClick={handlePrevious}>{'<'}</Button>
-        <Button onClick={handleNext}>{'>'}</Button>
-      </ButtonsWrapper>
-    </Wrapper>
+        <ButtonsWrapper>
+          <Button onClick={handlePrevious}>{'<'}</Button>
+          <Button onClick={handleNext}>{'>'}</Button>
+        </ButtonsWrapper>
+      </Wrapper>
+    </>
   );
 };
